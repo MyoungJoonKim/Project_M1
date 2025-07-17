e3d88f7cac64d7e284200b806fc40205348ea300
MyoungJoonKim <xxmj96@gmail.com>
2025-07-17 21:42:10 +0900

    7.17 Loading Scene Update - 로딩 시 배경 스프라이트, 게임팁 텍스트 랜덤 변경 구현

--- Assets/Scripts/UI/Scene_Loading.cs
+++ Assets/Scripts/UI/Scene_Loading.cs
@@ -7,8 +7,12 @@ using UnityEngine.UI;
 public class Scene_Loading : MonoBehaviour
 {
     public Slider LoadingBar;
+    public Text Texts;
+    public Image Background;
     public Sprite[] Images;
-    public Text[] Texts;
+
+    List<string> texts = new List<string>();
+
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +28,8 @@ public class Scene_Loading : MonoBehaviour
         while (true)
         {
             LoadingBar.value++;
+            RandomUpdateBackground();
+            RandomUpdateText();
             yield return new WaitForSeconds(1f);
 
             if (LoadingBar.value == timer)
@@ -32,5 +38,23 @@ public class Scene_Loading : MonoBehaviour
         SceneManager.LoadScene((int)Shared.Scene_Manager.NextScene);
     }
 
-    
+    public void RandomUpdateBackground()
+    {
+        int rand = Random.Range(0, Images.Length);
+        Background.sprite = Images[rand];
+    }
+
+    public void RandomUpdateText()
+    {
+        texts.Add("1.캐릭터 레벨업을 위해선 경험치가 필요합니다.");
+        texts.Add("2.스킬은 쿨타임이 있습니다.");
+        texts.Add("3.골드는 기본 재화입니다.");
+        texts.Add("4.몬스터를 잡으면 아이템을 획득할 수 있습니다.");
+        texts.Add("5.옵션에서 사운드를 조절할 수 있습니다.");
+
+        int rand = Random.Range(0, texts.Count);
+        Texts.text = texts[rand];
+    }
+
+
 }
