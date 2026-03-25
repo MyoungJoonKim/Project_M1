4abc65eed1b0cfab0b3ea405ac5b9ba131829ed0
MyoungJoonKim <xxmj96@gmail.com>
2026-03-25 21:34:31 +0900

    26.3.25 update
    몬스터 추가 (에셋 변경할 것)
    몬스터 오브젝트 풀 구현.
    
    플레이어 주변 안전구역 외 맵 범위에서 랜덤 생성.
    플레이어 태그 추적 이동 구현.
    리지드바디 충돌 구현. (hp 감소 효과 업데이트 필요)

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class Player_Controller : MonoBehaviour
 {
     public JoyStick JoyStick;
-    public float speed = 3f;
-    void Update()
+    private float speed = 3f;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
     {
         Vector2 input = JoyStick.Input;
-
-        Vector2 move = new Vector2 (input.x, input.y);
-        transform.Translate (move * speed * Time.deltaTime);
+        Vector2 move = input.normalized;
+        rb.velocity = move * speed;
     }
 }
