9254af141e2ac5d6ce57f3fb648117f8ed5e5a9c
MyoungJoonKim <xxmj96@gmail.com>
2026-03-29 17:26:31 +0900

    26.3.29 update
    몬스터 공격범위 체크 코루틴 구현 (콜라이더 충돌 -> 공격 범위 접근시 공격으로 변경)

--- Assets/02_Scripts/Characters/Enemy/Monster_Attack.cs
+++ Assets/02_Scripts/Characters/Enemy/Monster_Attack.cs
@@ -8,8 +8,9 @@ public class Monster_Attack : MonoBehaviour
     private Monster_Movement monster_Movement;
     private Monster_Animator monster_Animator;
 
-    public bool inAttackRange = false;
+    private Transform target;
     private Coroutine attackRoutine;
+    private float attackRange = 5f;
     private float attackCooldown = 1.5f;
     private float lastAttackTime = -999f;
 
@@ -17,11 +18,43 @@ public class Monster_Attack : MonoBehaviour
     {
         monster_Movement = GetComponent<Monster_Movement>();
         monster_Animator = GetComponent<Monster_Animator>();
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
 
         if (monster_Movement == null)
             Debug.Log("movement null");
         if (monster_Animator == null)
             Debug.Log("animator null");
+        if (target == null)
+            Debug.Log("target null");
+    }
+
+    private void Start()
+    {
+        StartCoroutine(AttackRange());
+    }
+
+    // 공격범위 검사 코루틴
+    IEnumerator AttackRange()
+    {
+        while (true)
+        {
+            float distance = Vector2.Distance(target.position, this.transform.position);
+
+            if (distance <= attackRange)
+            {
+                if (attackRoutine == null)
+                    attackRoutine = StartCoroutine(TryAttack());
+            }
+            else if (distance > attackRange)
+            {
+                if (attackRoutine != null)
+                {
+                    StopCoroutine(attackRoutine);
+                    attackRoutine = null;
+                }
+            }
+            yield return null;
+        }
     }
 
     // 공격 루프 코루틴
@@ -43,32 +76,4 @@ public class Monster_Attack : MonoBehaviour
     {
         monster_Animator.Attack();
     }
-
-    // 충돌 시 공격 루프 동작
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            inAttackRange = true;
-
-            if (attackRoutine == null)
-                attackRoutine = StartCoroutine(TryAttack());
-        }
-    }
-
-    // 충돌 중단 시 공격 루프 종료
-    void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            inAttackRange = false;
-
-            if (attackRoutine != null)
-            {
-                StopCoroutine(attackRoutine);
-                attackRoutine = null;
-            }
-        }
-    }
-    
 }
